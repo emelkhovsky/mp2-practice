dd6cf590c25faad8aa956d8c4744d31baa3dc782
emelkhovsky <emelkhovsky4@mail.ru>
2019-10-06 15:01:25 +0300

    add exceptions.h and tstack.h

---
+++ src/02_Stack/TStack.h
@@ -0,0 +1,78 @@
+#ifndef _TSTACK_H_ 
+#define _TSTACK_H_
+#include "exceptions.h"
+using namespace std;
+
+template<typename ValType>
+class TStack {
+private:
+	int max_size;
+	ValType* elements;
+	int top;
+public:
+	TStack(int);//конструктор
+	TStack(const TStack&);//конструктор копирования
+	~TStack();//деструктор
+	void Push(ValType);//кладет элемент на вершину стека
+	ValType Pop();//удаляем и выводим удаленный элемент
+	bool IsEmpty()const;//проверка на пустоту
+	bool IsFull()const;//проверка на полноту
+};
+
+template<typename ValType>//констркутор с параметром
+TStack<ValType>::TStack(int _max_size) {
+	if (_max_size <= 0) {
+		throw incorrect_max_size();
+	}
+	max_size = _max_size;
+	elements = new ValType[max_size];
+	top = 0;
+	for (int i = 0; i < max_size; i++) {
+		elements[i] = 0;
+	}
+}
+
+template<typename ValType>//конструктор копирования
+TStack<ValType>::TStack(const TStack& stack) {
+	max_size = stack.max_size;
+	top = stack.top;
+	for (int i; i < max_size; i++) {
+		elements[i] = stack.elements[i];
+	}
+}
+
+template<typename ValType>//деструктор
+TStack<ValType>::~TStack() {
+	top = 0;
+	max_size = 0;
+	delete [] elements;
+}
+
+template<typename ValType>//добавление элемента на вершину стека
+void TStack<ValType>::Push(ValType el) {
+	if (IsFull()) {
+		throw full();
+	}
+	elements[top] = el;
+}
+
+template<typename ValType>//удаление элемента и его вывод
+ValType TStack<ValType>::Pop() {
+	if (IsEmpty) {
+		throw empty();
+	}
+	ValType dop_value = dop_value + elements[top];
+	return dop_value;
+}
+
+template<typename ValType>//проверка на пустоту
+bool TStack<ValType>::IsEmpty()const{
+	return (top == 0);
+}
+
+template<typename ValType>//проверка на полноту
+bool TStack<ValType>::IsFull()const {
+	return (max_size == top);
+}
+
+#endif
---
+++ src/02_Stack/exceptions.h
@@ -0,0 +1,35 @@
+#ifndef _EXCEPTIONS_H_
+#define _EXCEPTIONS_H_
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+using namespace std;
+
+class incorrect_max_size : public std::exception {
+	const std::string what_Str = "Некорректный максимальный размер стека\n";
+public:
+	const char* what() const {
+		return what_Str.c_str();
+	}
+};
+
+class empty : public std::exception {
+	const std::string what_Str = "Стек пуст\n";
+public:
+	const char* what() const {
+		return what_Str.c_str();
+	}
+};
+
+class full : public std::exception {
+	const std::string what_Str = "Стек полон\n";
+public:
+	const char* what() const {
+		return what_Str.c_str();
+	}
+};
+
+
+#endif
