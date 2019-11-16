65deafc6bbc672c3703ca551b55cd6108fa4f57e
emelkhovsky <emelkhovsky4@mail.ru>
2019-11-16 14:11:49 +0300

    all is done, please3

--- src/02_Stack/calculator.h
+++ src/02_Stack/calculator.h
@@ -6,17 +6,17 @@ using namespace std;
 
 class TCalculator {
 private:
-    static int Priority(const char);//определение приоретета операций+(done)
-    static bool Comparison(char, TStack<char>&);//сравнение приоритетов
-    static bool IsItOperation(const char);//определение операция это или нет+(done)
+	static int Priority(const char);//определение приоретета операций
+	static bool Comparison(char, TStack<char>&);//сравнение приоритетов
+	static bool IsItOperation(const char);//определение операция это или нет
 public: 
-    static string PostfixForm(string);//образование постфиксной формы+
-    static int GettingCount(string);//возвращает кол-во операндов+(done)
-    static double Calculate(double*, string&, string);//возвращает результат подсчета+
-    static void GettingValues(double*, string&, string, int);//ввод значений+(done)
+    static string PostfixForm(string);//образование постфиксной формы
+    static int GettingCount(string);//возвращает кол-во операндов
+    static double Calculate(double*, string&, string);//возвращает результат подсчета
+    static void GettingValues(double*, string&, string, int);//ввод значений
 };
 
-//определение приоретета операций(done)
+//определение приоретета операций
  int TCalculator:: Priority(const char sign) {
     switch (sign) {
     case '+': return 2;
@@ -27,14 +27,14 @@ public:
     }
 }
  bool TCalculator::Comparison(char exp, TStack<char>& pop_el){//сравнение приоритетов 
-     return (Priority(pop_el.Pop_Get()) < Priority(exp));
+	 return (Priority(pop_el.Pop_Get()) < Priority(exp));
 
  };
-//определение операция это или нет(done)
+//определение операция это или нет
  bool TCalculator::IsItOperation(const char sign) {
     return ((sign == '*') || (sign == '/') || (sign == '+') || (sign == '-'));
 }
-//возвращает кол-во неповторяющих операндов(done)
+//возвращает кол-во неповторяющих операндов
  int TCalculator::GettingCount(string postfix_form) {
     int count = 0;
     for (int i = 0; i < postfix_form.length(); i++) {
@@ -46,34 +46,31 @@ public:
 }
 //ввод значений
  void TCalculator::GettingValues(double* values, string& operands, string p_f, int count) {
-     int current_count_of_operands = 0;
-     double value = 0;
-     char* new_operands = new char[count];
-     double* new_values = new double[count];
-     for (int i = 0; i < p_f.length(); i++) {
-         if (isalpha(p_f[i])) {
-             int flag = 0;
-             for (int j = 0; j < current_count_of_operands; j++) {
-                 if (new_operands[j] == p_f[i]) {
-                     flag = 1;
-                     break;
-                 }
-             }
-             if (flag == 0) {
-                 new_operands[current_count_of_operands] = p_f[i];
-                 cout << "введите значение операнда " << p_f[i] << endl;
-                 cin >> value;
-                 new_values[current_count_of_operands] = value;
-                 current_count_of_operands++;
-             }
-         }
-     }
-     operands.assign(new_operands);//скопировали 2ое в 1ое, преобразовали к string
-     memcpy(values, new_values, sizeof(double) * count);//скопировали 2ое в 1ое
+	 int current_count_of_operands = 0;
+	 double value = 0;
+	 char* new_operands = new char[count];
+	 double* new_values = new double[count];
+	 for (int i = 0; i < p_f.length(); i++) {
+		 if (isalpha(p_f[i])) {
+			 int flag = 0;
+			 for (int j = 0; j < current_count_of_operands; j++) {
+				 if (new_operands[j] == p_f[i]) {
+					 flag = 1;
+					 break;
+				 }
+			 }
+			 if (flag == 0) {
+				 new_operands[current_count_of_operands] = p_f[i];
+				 cout << "введите значение операнда " << p_f[i] << endl;
+				 cin >> value;
+				 new_values[current_count_of_operands] = value;
+				 current_count_of_operands++;
+			 }
+		 }
+	 }
+	 operands.assign(new_operands);//скопировали 2ое в 1ое, преобразовали к string
+	 memcpy(values, new_values, sizeof(double) * count);//скопировали 2ое в 1ое
 }
-//------------------------------------------------------------------------------------------------------------------
-
-
 
 //образование постфиксной формы
  string TCalculator::PostfixForm(string exp) {
@@ -84,48 +81,48 @@ public:
     TStack<char> stack2(exp.length() + 1);//операнды
     for (int i = 0; i < exp.length(); i++) {
         char sign = static_cast<char>(exp[i]);
-        if (sign == ' ') {
-            continue;
-        }
-        if (IsItOperation(sign)) {
-            if (stack1.IsEmpty()) {//если в стеке опраций еще ничего нет 
-                stack1.Push(sign);
-                continue;
-            }
-            if (Comparison(sign, stack1)) {//битва за приоритет
-                while (!stack1.IsEmpty()) {
-                    stack2.Push(stack1.Pop_Get());
-                    stack1.Pop();
-                }
-                stack1.Push(sign);
-            }
-            else
-                stack1.Push(sign);
-        }
+		if (sign == ' ') {
+			continue;
+		}
+		if (IsItOperation(sign)) {
+			if (stack1.IsEmpty()) {//если в стеке опраций еще ничего нет 
+				stack1.Push(sign);
+				continue;
+			}
+			if (Comparison(sign, stack1)) {//битва за приоритет
+				while (!stack1.IsEmpty()) {
+					stack2.Push(stack1.Pop_Get());
+					stack1.Pop();
+				}
+				stack1.Push(sign);
+			}
+			else
+				stack1.Push(sign);
+		}
 
-        if (sign == '(') {
-            stack1.Push(sign);
-        }
-        if (isalpha(sign)) {
-            stack2.Push(sign);
-        }
-        if (sign == ')') {
-            int left_bracket_flag = 0;
-            while (!stack1.IsEmpty()) {
-                if (stack1.Pop_Get() != '(') {
-                    cout << stack1.Pop_Get() << endl;
-                    stack2.Push(stack1.Pop_Get());
-                    stack1.Pop();
-                    continue;
-                }
-                stack1.Pop();
-                left_bracket_flag = 1;
-                break;
-            }
-            if ((left_bracket_flag != 1) && (stack1.IsEmpty())) {
-                throw Exception(" Кажется, вы забыли скобку (\n");
-            }
-        }
+		if (sign == '(') {
+			stack1.Push(sign);
+		}
+		if (isalpha(sign)) {
+			stack2.Push(sign);
+		}
+		if (sign == ')') {
+			int left_bracket_flag = 0;
+			while (!stack1.IsEmpty()) {
+				if (stack1.Pop_Get() != '(') {
+					cout << stack1.Pop_Get() << endl;
+					stack2.Push(stack1.Pop_Get());
+					stack1.Pop();
+					continue;
+				}
+				stack1.Pop();
+				left_bracket_flag = 1;
+				break;
+			}
+			if ((left_bracket_flag != 1) && (stack1.IsEmpty())) {
+				throw Exception(" Кажется, вы забыли скобку (\n");
+			}
+		}
 
     }
     while (!stack1.IsEmpty()){
@@ -134,13 +131,13 @@ public:
     }
     string postfix_form;
 
-    while (!stack2.IsEmpty()){
-        postfix_form += stack2.Pop_Get();
-        stack2.Pop();
-    }
+	while (!stack2.IsEmpty()){
+		postfix_form += stack2.Pop_Get();
+		stack2.Pop();
+	}
 
-    for (int i = 0; i < postfix_form.length() / 2; i++)
-        swap(postfix_form[i], postfix_form[postfix_form.length() - 1 - i]);
+	for (int i = 0; i < postfix_form.length() / 2; i++)
+		swap(postfix_form[i], postfix_form[postfix_form.length() - 1 - i]);
 
     return postfix_form;
 }
